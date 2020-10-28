14f1644e516459a1720a8ed822ba1ae27a329818
1goryan <1goryanichchmail@gmail.com>
2020-10-28 14:53:19 +0200

    add function input and small dialogue program

---
+++ 3.py
@@ -0,0 +1,36 @@
+                        #ВВОД Информации
+"""var = input()
+print(type(var))
+num1 = input()
+num2 = input()
+summ = num1 + num2
+print(summ)
+some1 = "22"
+some2 = "33"
+some1 = int(some1) #приведение к типу численная
+some2 = int(some2)
+summ = some1 + some2
+print(summ)
+
+some1 = "22"
+some2 = "33"
+some1 = float(some1) 
+some2 = float(some2)
+summ = some1 + some2
+print(summ)
+
+num1 = float(input())
+num2 = float(input())
+summ = num1 + num2
+print(summ) 
+
+print("What is your name?") #программа ведения диалога
+name = input()
+print("Hello " + name + "!")
+print("How old are you?")
+old = input()
+print("Ok, you are " + old + " years old")
+print("What's you hobby?")
+hobby = input()
+print(hobby + " is such an intresting hobby!")
+"""
