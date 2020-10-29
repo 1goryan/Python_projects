5cb07a83f75b878c429cf64089d0e3225ae0570c
1goryan <1goryanichchmail@gmail.com>
2020-10-29 18:28:35 +0200

    add logical expressions in python

---
+++ logical_expressions.py
@@ -0,0 +1,23 @@
+"""                    #Логика
+and (И)
+or  (ИЛИ)
+not (НЕ)
+!=  (не равно)
+==  (равно)
+>=  (больше или равно)
+<=  (меньше или равно)
+True    (Истина)
+False   (Ложь) """
+
+var0 = "some"
+var1 = True
+
+if var0 and var1:
+    print("done")
+
+var3 = 12
+var4 = 5
+if var3 == var4:
+    print("done")
+else :
+    print("dont")
