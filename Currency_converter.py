360045aeb9892b28f5015093c48d4dccf3c460b6
Ihor <71976060+1goryan@users.noreply.github.com>
2020-11-06 16:17:12 +0200

    Add files via upload
    Add converter function from UAH to USD, EUR, RUB, PLN, CHF, GBR

--- Currency_converter.py
+++ Currency_converter.py
@@ -3,17 +3,36 @@ from bs4 import BeautifulSoup
 
 url = 'https://minfin.com.ua/currency/nbu/'
 
-def convert_uah_to_usd (uah):
-    source = requests.get(url)
-    main_text = source.text
-    soup = BeautifulSoup(main_text)
+source = requests.get(url)
+main_text = source.text
+soup = BeautifulSoup(main_text,'html.parser')
 
-    table = soup.find('table', {'class' : 'table-auto'} ) # если find без All то получаем только первое совпадение
-    tr = table.find('td', {'class' : 'responsive-hide'})
-    tr = tr.text
-    tr = tr [:10]
-    doll = float(tr)
-    summ = uah / doll
-    print(summ)
+table = soup.find('table', {'class' : 'table-auto'} ) # если find без All то получаем только первое совпадение
+tr = table.findAll('td', {'class' : 'responsive-hide'})
+mn = []
+for f in tr:
+    h = f.text[1:7]
+    mn.append(h)
+      
+for i in range(len(mn)):
+    mn[i] = float(mn[i])
 
-convert_uah_to_usd(float(input()))
+def converter (uah, currency):
+    if currency == "USD":
+        course = uah / mn[0]
+    elif currency == "EUR":
+        course = uah / mn[1]
+    elif currency == "RUB":
+        course = uah / mn[2]
+    elif currency == "PLN":
+        course = uah / mn[3]
+    elif currency == "CHF":
+        course = uah / mn[4]
+    elif currency == "GBR":
+        course = uah / mn[5]
+    else:
+        print("please select the correct currency") 
+
+    print(str(uah) + " UAH = " + str(course) + " " + currency)
+
+converter(float(input("Write your sum of UAH ")), str(input("Available currency: USD, EUR, RUB, PLN, CHF, GBR ")))
