2b971011e78bf77a8b4a8ee0a9652e887badd7f8
Ihor <71976060+1goryan@users.noreply.github.com>
2020-11-25 19:18:41 +0200

    add movement funct to ball, hitting a ball with a racket, score table and racket movement limits

--- Game.py
+++ Game.py
@@ -18,7 +18,7 @@ racket_a.goto(-350, 0)
 racket_b = turtle.Turtle() #создаем экземпляр класса
 racket_b.speed(0) #задаем скорость элемента
 racket_b.shape("square") #задаем форму
-racket_b.color("yellow") #задаем цвет
+racket_b.color("green") #задаем цвет
 racket_b.shapesize(stretch_len=1, stretch_wid=5) #задаем размеры экземпляра
 racket_b.penup() #метод для перемещения ээкземпляра
 racket_b.goto(350, 0)
@@ -31,28 +31,55 @@ ball.shape("circle")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 0.2 #скорость движения шарика по х
+ball.dy = 0.2  #скорость движения шарика по у
+
+#создаем счет
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("red")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0 || Player B: 0", align = "center", font = ("Verdana", 22, "normal"))
+
+score_a = 0
+score_b = 0
 
 #реализация движения ракеток
 
 def racket_a_up():
     y = racket_a.ycor() #метод записывает в переменную у местоположение ракетки
-    y += 20 #переместили ракетку_a на 20 пикселей вверх
-    racket_a.sety(y) # задаем новое значение y
+    if y > 225:
+        racket_a.goto(-350, 245)
+    else:
+        y += 20 #переместили ракетку_a на 20 пикселей вверх
+        racket_a.sety(y) # задаем новое значение y
 
 def racket_a_down():
     y = racket_a.ycor() #метод записывает в переменную у местоположение ракетки
-    y -= 20 #переместили ракетку_a на 20 пикселей вниз
-    racket_a.sety(y) # задаем новое значение y
+    if y < -225:
+        racket_a.goto(-350, -245)
+    else:
+        y -= 20 #переместили ракетку_a на 20 пикселей вниз
+        racket_a.sety(y) # задаем новое значение y
 
 def racket_b_up():
     y = racket_b.ycor() #метод записывает в переменную у местоположение ракетки
-    y += 20 #переместили ракетку_b на 20 пикселей вверх
-    racket_b.sety(y) # задаем новое значение y
+    if y > 225:
+        racket_b.goto(350, 245)
+    else:
+        y += 20 #переместили ракетку_b на 20 пикселей вверх
+        racket_b.sety(y) # задаем новое значение y
 
 def racket_b_down():
     y = racket_b.ycor() #метод записывает в переменную у местоположение ракетки
-    y -= 20 #переместили ракетку_b на 20 пикселей вниз
-    racket_b.sety(y) # задаем новое значение y
+    if y < -225:
+        racket_b.goto(350, -245)
+    else:
+        y -= 20 #переместили ракетку_b на 20 пикселей вниз
+        racket_b.sety(y) # задаем новое значение y
 
 #привязка к клавиатуре
 win.listen()
@@ -61,7 +88,37 @@ win.onkeypress(racket_a_down,"s")
 win.onkeypress(racket_b_up,"Up")
 win.onkeypress(racket_b_down,"Down")
 
-
-
 while True:
     win.update()
+    #движение шарика
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1   
+
+    if ball.xcor() > 390: #если не отбили шарик - он стартует с центра и движется в другую сторону
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {} || Player B: {}".format(score_a, score_b), align = "center", font = ("Verdana", 22, "normal"))
+        
+    if ball.xcor() < -390:#если не отбили шарик - он стартует с центра и движется в другую сторону
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {} || Player B: {}".format(score_a, score_b), align = "center", font = ("Verdana", 22, "normal"))
+        #проверка если позиция мячика >340 проверяем позицию правой рактки, если мячик попал в ракетку, то мы его отбили 
+    if ball.xcor() > 340 and ball.ycor() < racket_b.ycor() + 50 and ball.ycor() > racket_b.ycor() -50:
+        ball.dx *= -1
+        #проверка если позиция мячика >340 проверяем позицию правой рактки, если мячик попал в ракетку, то мы его отбили, 
+    if ball.xcor() < -340 and ball.ycor() < racket_a.ycor() + 50 and ball.ycor() > racket_a.ycor() -50:
+        ball.dx *= -1
+    
